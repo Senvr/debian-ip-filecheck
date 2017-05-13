98263fda2c4d5e4890a70d48ab6bde92cdbc0748
Lee Ikard <Leeeyecard@gmail.com>
2017-05-12 23:17:50 -0400

    Add files via upload

--- IPFC.sh
+++ IPFC.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
+Time="$(date +"%m-%d-%y")"
+echo $Time"Set the time" > $DIR/debug.txt
 mkdir -p Temp
-if [[ ! $EUID -ne 0 ]]; then
-   echo "This script cannot be ran as root." 
-   exit 1
-fi
-which git &> /dev/null || { echo >&2 "I require git but it's not installed.  Aborting."; ExitError 1; }
+echo $Time"Made the directory Temp" >> $DIR/debug.txt
+
+echo $Time"Checked GIT version" >> $DIR/debug.txt
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd  )"
 ExitError(){
   echo "Program exited with an error! Check the debug file. Severity: $1"
@@ -14,16 +14,35 @@ ExitError(){
   rm *
   exit $ErrorCode
 }
+echo $Time"Set the ExitError function" >> $DIR/debug.txt
+
+echo "Reading config...." >&2
+. $DIR/Config.cfg
+source $DIR/Config.cfg
+if [ "$Auto" = "True" ]; then
+  FillVar=True
+fi
+SourceFile=$1
+if [ "$FillVar" = "True" ]; then
+   if [ -z $SourceFile ]; then
+	SourceFile="/var/log/auth.log"
+fi
+fi
+echo $SourceFile
 cd $DIR/Temp
+echo $Time"CD to dir var" >> $DIR/debug.txt
 wget https://raw.githubusercontent.com/Senvr11/debian-ip-filecheck/master/Files/VERSION
+echo $Time"get github file" >> $DIR/debug.txt
 UpVer=$(cat VERSION)
+echo $Time"check" >> $DIR/debug.txt
 Version=$(cat $DIR/Files/VERSION)
+echo $Time"im lasy" >> $DIR/debug.txt
 if [ ! "$UpVer" = "$Version" ]; then
 bash $DIR/Files/Update.sh
 else
 echo "No update required."
 fi
-
+which git &> /dev/null || { echo >&2 "I require git but it's not installed.  Aborting."; ExitError 1; }
 cd $DIR
 Version=$(cat $DIR/Files/VERSION)
 platform='unknown'
@@ -38,14 +57,13 @@ else
    echo "WARNING: PROGRAM CANNOT DETERMINE PLATFORM. I DO NOT RECOMMEND RUNNING! YOU HAVE BEEN WARNED."
    sleep 10
 fi
-SourceFile=$1
 if [ ! -f $SourceFile ]; then
     File="False"
     FileError="file doesn't exist."
 fi
 if [ ! -r $SourceFile ]; then
 	File="False"
-	FileError="file could not be read"
+	FileError="file could not be read. check permissions"
 fi	
 if [ ! -s $SourceFile ]; then
 	File="False"
@@ -54,21 +72,8 @@ fi
 
 
 
-cat >$DIR/debug.txt <<EOL
-Variables:
-================
-$SourceFile
-$DIR
-$Version
-$platform
-$unamestr
-$RealFile
-$File
-=================
-Read: $SourceFile. Success: $File. Error (if any): $FileError
-=================
-End debug
-EOL
+
+echo $Time" " >> $DIR/debug.txt
 cd $DIR
 clear
 echo "IP.F.C, version $Version. Created by Senvr11"
@@ -78,7 +83,22 @@ if [[ $File == "False" ]]; then
   ExitError 1
 fi
 ###Scraping file for IPs
-IPs=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' $SourceFile)
-cd $DIR
-echo $IP > $DIR/Temp/TempFile.A
+fileA=$DIR/Temp/TempFile.A.txt
+if [ ! -e "$fileA" ] ; then
+    touch "$fileA"
+fi
+
+if [ ! -w "$fileA" ] ; then
+    echo cannot write to $file
+    ExitError 2
+fi
+echo $Auto
+if [ "$Auto" = "False" ]; then
+ echo "Please enter the file you would like to scan! (To disable this feature, go to $DIR/Config.cfg and turn "Auto" to False. (CASES MATTER!!!!)"
+ read SourceFile
+fi
+
+sudo grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" $SourceFile > $DIR/Temp/TempFile.A.txt
+cd $DIR/Temp
+perl -ne 'print if ! $a{$_}++' $DIR/Temp/TempFile.A.txt > $DIR/Temp/TempFIle.B.txt
 
